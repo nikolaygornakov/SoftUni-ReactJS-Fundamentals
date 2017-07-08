848a3f94c1cb7c1b4fda933bf08a1b4e5fc66dde
nikolaygornakov <nikolaygornakov@yahoo.com>
2017-07-08 18:12:48 +0300

    Prepare app for routing and added simple Navbar component

--- Workshop-Exercise/pets-system/src/App.js
+++ Workshop-Exercise/pets-system/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import Navbar from './components/common/Navbar'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        Test
+        <Navbar />
       </div>
     )
   }
