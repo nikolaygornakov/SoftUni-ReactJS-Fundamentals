b7950b49a48f6aef55f17e88fc059f1f213d5002
nikolaygornakov <nikolaygornakov@yahoo.com>
2017-07-08 18:36:44 +0300

    Added Routes component and simple HomePage component

--- Workshop-Exercise/pets-system/src/App.js
+++ Workshop-Exercise/pets-system/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Navbar from './components/common/Navbar'
+import Routes from './components/common/routes/Routes'
 import './App.css'
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Navbar />
+        <Routes />
       </div>
     )
   }
