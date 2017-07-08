ebc394177eaafc7eb55078270749ceeae664b5c4
nikolaygornakov <nikolaygornakov@yahoo.com>
2017-07-08 19:36:13 +0300

    Added Users Register and Login pages. Added links in Navbar component

--- Workshop-Exercise/pets-system/src/components/common/Navbar.js
+++ Workshop-Exercise/pets-system/src/components/common/Navbar.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Navbar extends Component {
   render () {
     return (
-      <div>
-        Navbar
+      <div className='menu'>
+        <Link to='/'>Home</Link>
+        <Link to='/users/register'>Register</Link>
+        <Link to='/users/login'>Login</Link>
       </div>
     )
   }
--- Workshop-Exercise/pets-system/src/components/common/routes/Routes.js
+++ Workshop-Exercise/pets-system/src/components/common/routes/Routes.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import HomePage from '../../HomePage'
+import RegisterPage from '../../users/RegisterPage'
+import LoginPage from '../../users/LoginPage'
 
 const Routes = () => (
   <Switch>
     <Route path='/' exact component={HomePage} />
+    <Route path='/users/register' component={RegisterPage} />
+    <Route path='/users/login' component={LoginPage} />
   </Switch>
 )
 
